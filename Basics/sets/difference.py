4073f6e3b1c195def64136c41d66d262cd5feee2
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2023-06-27 19:51:13 +0300

    Create difference.py

---
+++ Basics/sets/difference.py
@@ -0,0 +1,12 @@
+a = {10, 'ali', 20, 'veli', 'selami', 30}
+b = {30,'veli', 'ayşe', 40, 20, 'fatma'}
+
+c = a - b
+print(c)
+
+c = a.difference(b)
+print(c)
+
+x = [10, 'ali', 'selami']
+c = a.difference(x)
+print(c)
