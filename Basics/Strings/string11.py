abb0bf9d1004786ef0b853463b772077751bf349
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2024-01-05 22:53:36 +0300

    join method concatenates strings

---
+++ Basics/Strings/string11.py
@@ -0,0 +1,11 @@
+s = 'xxx'
+names = ['ali', 'veli', 'selami']
+print(s.join(names))
+s = ' '
+print(s.join(names))
+s = ', '
+print(s.join(names))
+s = ''
+print(s.join(names))
+print('\n'.join(names))
+print(', '.join(dir(str)))
