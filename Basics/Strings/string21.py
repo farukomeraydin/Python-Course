95b449d79857183e7150f3cab29e33739ebc6761
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2024-01-05 23:10:16 +0300

    endswith

---
+++ Basics/Strings/string21.py
@@ -0,0 +1,13 @@
+s = 'ankara'
+
+result = s.endswith('ra')
+print(result)
+
+result = s.endswith('kara')
+print(result)
+
+result = s.endswith('an')
+print(result)
+
+result = s.endswith('ankara')
+print(result)
