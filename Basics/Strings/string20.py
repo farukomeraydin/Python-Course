f118fadd52a8eccf340295b3d3c1365f7e0c8532
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2024-01-05 23:09:27 +0300

    startswith

---
+++ Basics/Strings/string20.py
@@ -0,0 +1,13 @@
+s = 'ankara'
+
+result = s.startswith('an')
+print(result)
+
+result = s.startswith('anka')
+print(result)
+
+result = s.startswith('anki')
+print(result)
+
+result = s.startswith('ankara')
+print(result)
