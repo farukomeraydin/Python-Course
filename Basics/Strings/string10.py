4260a15f42083a638a0322600c497cfdb8f9db33
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2024-01-05 22:51:27 +0300

    isXXX functions

---
+++ Basics/Strings/string10.py
@@ -0,0 +1,19 @@
+s = 'ağrı-04'
+k = 'AĞRIDağı'
+m = '12393456789'
+r = '       '
+
+result = s.islower()
+print(result)
+
+result = k.isupper()
+print(result)
+
+result = m.isdigit()
+print(result)
+
+result = k.isalpha()
+print(result)
+
+result = r.isspace()
+print(result)
