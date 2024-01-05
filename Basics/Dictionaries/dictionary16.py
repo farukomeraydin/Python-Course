b73452dbb7dfd59a4b34e8592d483a92dc3dd0ef
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2024-01-05 22:26:32 +0300

    keys and values method gives view object

---
+++ Basics/Dictionaries/dictionary16.py
@@ -0,0 +1,17 @@
+d = {'ali':10, 'veli':20, 'selami':30, 'ayşe':40, 'fatma':50}
+
+keys = d.keys()
+a = list(keys)
+print(a)
+
+values = d.values()
+a = list(values)
+print(a)
+
+d[60] = 'nurettin'
+
+a = list(keys)
+print(a)
+
+a = list(values)
+print(a)
