86f11625267b808bcd692ae1b84c4da8ec0272f1
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2024-01-08 19:46:36 +0300

    sequence type pattern

---
+++ Basics/python_v3_10/match5.py
@@ -0,0 +1,15 @@
+while True:
+    cmd = input('CSD>').split()
+    if cmd == '':
+        continue
+    match cmd:
+        case ['copy']:
+            print('copy executes')
+        case ['rename']:
+            print('rename executes')
+        case (['del'] | ['erase'] | ['remove']) as del_cmd:
+            print(f'{del_cmd[0]} executes')
+        case ['quit'] | ['exit']:
+            break
+        case _:
+            print(f'invalid command: {cmd[0]}')
